17873a6190bf58624da8ce16290ba2b8811d7fec
Fergus Noble <fergusnoble@gmail.com>
2016-02-27 21:32:56 -0800

    Transmit mode — ENHANCE!

--- experiments/FT232H/test1.py
+++ experiments/FT232H/test1.py
@@ -56,28 +56,31 @@ def print_temp(radio):
   radio.set_register(REG.FSK.IMAGE_CAL, image_cal)
 
   # 6) Set device back to Sleep mode
-  radio.set_mode(MODE.SLEEP)  
-  
+  radio.set_mode(MODE.SLEEP)
+
   # 7) Access temperature value in RegTemp (0x3C)
   val = radio.get_register(0x3C)
-  print val 
+  print val
 
-def transmit(radio, msg=[0x50, 0x69, 0x6E, 0x67]):
+def transmit(radio, msg="PING"):
 
+  max_len = radio.get_max_payload_length()
+  # Truncate to maximum payload size
+  # TODO: Loop to support longer messages
+  msg = msg[:max_len]
+
+  msg_ = map(ord, msg)
   radio.set_mode(MODE.STDBY)
   radio.set_pa_config(pa_select=0)
-  radio.write_payload(msg)
-  radio.set_mode(MODE.TX)  # send PING
-  print "send PING"
+  radio.set_payload_length(len(msg_))
+  radio.write_payload(msg_)
+  radio.set_mode(MODE.TX)
 
-  print "IRQ Flags:"
-  print radio.get_irq_flags()
+  # Wait for tx_done
+  while radio.get_irq_flags()['tx_done'] != 1:
+      time.sleep(0.1)
 
-  time.sleep(1)
-
-  print "IRQ Flags:"
-  print radio.get_irq_flags()
-
+  radio.set_mode(MODE.STDBY)
 
 def receive(radio):
   radio.reset_ptr_rx()
@@ -103,30 +106,39 @@ def main():
 
   FT232H.use_FT232H()
   ft232h = FT232H.FT232H()
-   
+
   # Create a SPI interface from the FT232H using pin 8 (C0) as chip select.
   # Use a clock speed of 3mhz, SPI mode 0, and most significant bit first.
   spi = FT232H.SPI(ft232h, cs=8, max_speed_hz=3000000, mode=0, bitorder=FT232H.MSBFIRST)
 
 
-  radio = LoRa(spi=spi)
+  radio = LoRa(spi=spi, verbose=False)
   radio.set_freq(915)
 
-  print(radio)
+  #print radio
 
-  print "IRQ Flags:"
-  print radio.get_irq_flags()
+  a = raw_input("Transmit [T], Ping [P] or Receive [R]? ")
 
-  a = raw_input("Transmit [T] or Receive [R]? ")
-
-  if a == "T":
-    transmit(radio)
+  if a == "P":
+    try:
+      while True:
+        msg = "Ping: " + time.asctime()
+        print msg
+        transmit(radio, msg)
+        time.sleep(1)
+    except KeyboardInterrupt:
+      pass
+  elif a == "T":
+    try:
+      while True:
+        msg = raw_input("Message:")
+        transmit(radio, msg=msg)
+    except KeyboardInterrupt:
+      pass
   elif a == "R":
     receive(radio)
   else:
     print "Unknown command:", a
-
-
 
 if __name__ == "__main__":
   main()
@@ -140,7 +152,7 @@ if __name__ == "__main__":
 # # Read 3 bytes of data using the SPI protocol.
 # response = spi.read(3)
 # print 'Received {0}'.format(response)
- 
+
 # # Write 3 bytes and simultaneously read 3 bytes using the SPI protocl.
 # response = spi.transfer([0x01, 0x02, 0x03])
 # print 'Received {0}'.format(response)
